5d3002084b768ab52a0af5de46594ae97e60510e
Omer Gazit <omer@algolands.com>
2026-06-15 07:58:02 +0100

    first versin

---
+++ script.js
@@ -0,0 +1,7 @@
+function openWhatsApp(lang){
+  const phone='351933221858';
+  const text = lang === 'he'
+    ? 'היי! אני רוצה להצטרף לסיור החינמי בפורטו בעברית.%0Aתאריך:%0Aמספר משתתפים:%0Aשם:'
+    : 'Hi! I want to join the free walking tour in Porto in English.%0ADate:%0ANumber of people:%0AName:';
+  window.open(`https://wa.me/${phone}?text=${text}`,'_blank');
+}
